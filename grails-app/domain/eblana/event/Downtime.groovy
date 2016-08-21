7b7ee05e2a6e3a8cc061299afb1de51c3d42fdfb
Hugh <hughlardner@gmail.com>
2016-08-21 19:40:56 +0100

    fix to current crystal count

--- grails-app/domain/eblana/event/Downtime.groovy
+++ grails-app/domain/eblana/event/Downtime.groovy
@@ -43,27 +43,27 @@ class Downtime {
 	static transients = ['airCurrent', 'earthCurrent', 'fireCurrent', 'waterCurrent','blendedCurrent','voidCurrent','itemCurrent']
 	
 	public int getAirCurrent(){
-		return airCrystals?:0 + to*.air?.sum(0) - craftLog*.airCrystals?.sum(0) - from*.air?.sum(0)
+		return (airCrystals?:0) + to*.air?.sum(0) - craftLog*.airCrystals?.sum(0) - from*.air?.sum(0)
 	}
 	
 	public int getEarthCurrent(){
-		return earthCrystals?:0 + to*.earth?.sum(0) - craftLog*.earthCrystals?.sum(0) - from*.earth?.sum(0)
+		return (earthCrystals?:0) + to*.earth?.sum(0) - craftLog*.earthCrystals?.sum(0) - from*.earth?.sum(0)
 	}
 	
 	public int getFireCurrent(){
-		return fireCrystals?:0 + to*.fire?.sum(0) - craftLog*.fireCrystals?.sum(0) - from*.fire?.sum(0)
+		return (fireCrystals?:0) + to*.fire?.sum(0) - craftLog*.fireCrystals?.sum(0) - from*.fire?.sum(0)
 	}
 	
 	public int getWaterCurrent(){
-		return waterCrystals?:0 + to*.water?.sum(0) - craftLog*.waterCrystals?.sum(0) - from*.water?.sum(0)
+		return (waterCrystals?:0) + to*.water?.sum(0) - craftLog*.waterCrystals?.sum(0) - from*.water?.sum(0)
 	}
 	
 	public int getBlendedCurrent(){
-		return blendedCrystals?:0 + to*.blended?.sum(0) - craftLog*.blendedCrystals?.sum(0) - from*.blended?.sum(0)
+		return (blendedCrystals?:0) + to*.blended?.sum(0) - craftLog*.blendedCrystals?.sum(0) - from*.blended?.sum(0)
 	}
 	
 	public int getVoidCurrent(){
-		return voidCrystals?:0 + to*.voidC?.sum(0) - craftLog*.voidCrystals?.sum(0) - from*.voidC?.sum(0)
+		return (voidCrystals?:0) + to*.voidC?.sum(0) - craftLog*.voidCrystals?.sum(0) - from*.voidC?.sum(0)
 	}
 	
 	public Set<Item> getItemCurrent(){
