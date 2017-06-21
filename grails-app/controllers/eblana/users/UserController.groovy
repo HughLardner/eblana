5d34d3d37d694237eff73b49f2c3bd84956dd5f0
Hugh <hlardner@salesforce.com>
2017-06-21 13:35:08 +0100

    Remove ref to User

--- grails-app/controllers/eblana/users/UserController.groovy
+++
@@ -1,7 +0,0 @@
-package eblana.users
-
-import grails.plugin.springsecurity.annotation.Secured
-
-@Secured(['ROLE_ADMIN'])
-class UserController extends grails.plugin.springsecurity.ui.UserController {
-}
